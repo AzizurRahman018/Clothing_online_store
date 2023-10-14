0a645786ae3ddb3825f39bae10c6da089159a015
Md Azizur Rahman <106616550+AzizurRahman018@users.noreply.github.com>
2023-10-14 12:41:21 +0600

    slider

--- shop_app/models.py
+++ shop_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Slider (models.Model):
+    title= models.CharField(max_length=50)
+    title1= models.CharField(max_length=50)
+    description= models.TextField()
+    button_tag= models.CharField(max_length=20)
+    image= models.ImageField(upload_to='slider_image/')
+
+    def __str__(self):
+        return str(self.title)
+
+class Catagory(models.Model):
+
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.title)
