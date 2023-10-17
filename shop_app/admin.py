8170491ef9867e73c08923129c63383e2d7e6790
Md Azizur Rahman <106616550+AzizurRahman018@users.noreply.github.com>
2023-10-17 19:03:50 +0600

    model update

--- shop_app/admin.py
+++ shop_app/admin.py
@@ -3,3 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 admin.site.register(Slider)
+admin.site.register(Product)
+admin.site.register(SubCategory)
+admin.site.register(SIZE)
+admin.site.register(CONDITION)
+admin.site.register(COLOR)
