53e3f9940c4062e8d5bf7f70b64f1d522e20a71d
Md Azizur Rahman <106616550+AzizurRahman018@users.noreply.github.com>
2023-10-30 00:24:05 +0600

    super sub cata

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-path('',Home,name='home')
+path('',Home,name='home'),
+
+path('super_sub_prod/<int:id>/',super_sub_prod,name='super_sub_prod')
 ]
--- shop_app/views.py
+++ shop_app/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q
+
 # Create your views here.
 def Home(request):
-
+    if request.method == 'GET':
+        search = request.GET.get('search')
+        if search:
+            Products = Product.objects.filter(Q(title__icontains = search ))
+        else:
+            Products = Product.objects.all()
     slides = Slider.objects.all()
     Products = Product.objects.all()
-    feture_pro= Products.filter(featured_product = True)
-    trending_pro=Products.filter(trending_product = True)
-    top_seller=Products.filter(top_seller = True)
-    deals_of_the_day=Products.filter(deals_of_the_day = True)
-    return render(request,'Home.html',{'slides':slides,'feture_pro':feture_pro,'trending_pro':trending_pro,'top_seller':top_seller,'deals_of_the_day':deals_of_the_day})
+    feture_pro = Products.filter(featured_product=True)
+    trending_pro = Products.filter(trending_product=True)
+    top_seller = Products.filter(top_seller=True)
+    deals_of_the_day = Products.filter(deals_of_the_day=True)
+
+    return render(request, 'Home.html',
+                  {'slides': slides, 'feture_pro': feture_pro, 'trending_pro': trending_pro, 'top_seller': top_seller,
+                   'deals_of_the_day': deals_of_the_day})
+def super_sub_prod(request, id):
+    prod = Product.objects.filter(super_sub_Category=id)
+    print(prod)
+    slides = Slider.objects.all()
+
+
+    return render(request, 'Product/super_sub_prod.html',{'prod': prod,'slides': slides })
